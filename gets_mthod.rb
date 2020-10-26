2641fe9ccc56dfec1a7f1940b86616c567f0a2d6
gbarhorst <70030575+gbarhorst@users.noreply.github.com>
2020-10-26 12:56:16 -0400

    Updates

--- gets_mthod.rb
+++ gets_mthod.rb
@@ -2,4 +2,6 @@ puts "Hi, what's your name?"
 name = gets.chomp
 
 puts "Great! What's your age?"
-age = gets.chomp
+age = gets.chomp.to_i
+
+puts "Cool, so your name is #{name} and you are #{age} years old."
