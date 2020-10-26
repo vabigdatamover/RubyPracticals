854c35f1e4217c23fbab568c8b9c0ada7091e6dd
gbarhorst <70030575+gbarhorst@users.noreply.github.com>
2020-10-26 15:49:53 -0400

    Updates

--- intro_to_numbers.rb
+++ intro_to_numbers.rb
@@ -15,3 +15,22 @@ p pi.to_i
 p pi.to_i.class
 p pi.to_s
 p pi.to_s.class
+puts
+p 10 == 10
+p 10 == 20
+
+a = 10
+b = 5
+c = 10
+
+p a == c
+p b == b
+p a == c
+
+p -10.5.between?(-20, 0)
+p 5.3.between?(8.8, 9.6)
+puts
+p 10.5.floor
+p 10.5.floor.class
+p 10.5.ceil
+p 10.5.ceil.class
