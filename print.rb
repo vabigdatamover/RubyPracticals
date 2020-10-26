b04d74baa2177c500f21300380fa8f7d3d604976
gbarhorst <70030575+gbarhorst@users.noreply.github.com>
2020-10-26 11:17:26 -0400

    First Commit

---
+++ print.rb
@@ -0,0 +1,16 @@
+print "Hello"
+print "World"
+print " Hello "
+print "World"
+print " "
+print "Hello"
+print " "
+print "World"
+puts "Steven Seagal"
+p "Steven Seagal"
+
+puts "Hi there, this is a
+big line break"
+
+p "Hi there, this is a
+big line break"
---
+++ variables.rb
@@ -0,0 +1,22 @@
+name = "Boris"
+last_name = "Paskhaver"
+handsome = true
+age = 24 + 1
+
+puts name
+puts last_name
+puts name + " "  + last_name
+puts handsome
+puts age - 1
+
+
+# Parallel Varaible assignment
+
+a = 10
+b =20
+c = 30
+puts
+p a,b,c
+puts
+d,e,f = 10,20,30
+p d,e,f
